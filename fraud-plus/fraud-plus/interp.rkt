0b4cb3ce9f282dea3b57a587a28128ef586e39a5
yh-ki <84252233+yh-ki@users.noreply.github.com>
2022-03-31 12:43:21 -0400

    Add files via upload

--- fraud-plus/fraud-plus/interp.rkt
+++ fraud-plus/fraud-plus/interp.rkt
@@ -53,9 +53,9 @@
        ['err 'err]
        [v    (interp-env e2 r)])]
     ;; TODO: implement cond
-    [(Cond cs e) (interp-cond cs e)]
+    [(Cond cs e) (interp-cond cs e r)]
     ;; TODO: implement case
-    [(Case ev cs el) (interp-case e cs el)]
+    [(Case ev cs el) (interp-case e cs el r)]
     ;; TODO: this works for just a single binding
     ;; but you need to make it work in general
     [(Let (list x) (list e1) e2)
@@ -113,7 +113,7 @@
        [(cons v vs)
         (let ((y (assoc x r))) (if y (cons (exts r xs vs) (cons (list x v) (remove y r))) (cons (exts r xs vs) (cons (list x v) r))))])]))
 
-(define (interp-cond cs e)
+(define (interp-cond cs e r)
   (match cs
        ['() (interp-env e r)]
        [(list (Clause e1 e2) x ...) (if (interp-env e1 r) (interp-env e2 r) (interp-cond x e))]))
